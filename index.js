dc55b5d2907c1d743e7413cdfb981ababa69198e
Irfan Hanjra <ihanjra@gmail.com>
2023-02-14 13:44:49 -0500

    Initial commit

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+
+
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const { name, planet } = moon;
+
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -31,13 +33,12 @@ function organizeMoonsByPlanet(moons) {
 /*
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
-  - Use object destructuring and the rest operator to assign the `closest` and `rest` variables.
+  - Use array destructuring and the rest operator to assign the `closest` and `rest` variables.
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
 
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
@@ -56,20 +57,20 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
 
   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
 
-  return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
-  };
-}
+ return { name, 
+    planet, 
+    distanceFromPlanetInKm };
+  
+};
 
+  
 module.exports = {
   organizeMoonsByPlanet,
   getClosestToPlanet,
