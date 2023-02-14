3163722461fec4bb1bf3ab5759e9610859d1b420
Jennifer <jenniferpeterson@pursuit.org>
2023-02-14 12:44:14 -0500

    function one complete

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+// function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const { name, planet } = moon;
+
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -26,7 +27,7 @@ function organizeMoonsByPlanet(moons) {
     }
   }
   return result;
-}
+};
 
 /*
   To update this function, you should:
