7daeba29682221eb4d2fb6766318a98b0c7a1cc3
lgxxhi <luisgsilva95@gmail.com>
2023-02-15 00:58:59 -0500

    Finished Lab

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const moons = [
 
   You should not need to make any other changes to the code.
 */
-function organizeMoonsByPlanet(moons) {
+const organizeMoonsByPlanet = (moons) => {
   const result = {};
   for (let moon of moons) {
-    const name = moon.name;
-    const planet = moon.planet;
+    const { name, planet } = moon;
     if (result[planet]) {
       result[planet].push(name);
     } else {
@@ -26,27 +25,24 @@ function organizeMoonsByPlanet(moons) {
     }
   }
   return result;
-}
+};
 
 /*
   To update this function, you should:
   - Change the function to use arrow syntax, while keeping the name of the function the same.
-  - Use object destructuring and the rest operator to assign the `closest` and `rest` variables.
+  - Use array destructuring and the rest operator to assign the `closest` and `rest` variables.
 
   You should not need to make any other changes to the code.
 */
-function getClosestToPlanet(moons) {
-  let closest = moons[0];
-  const rest = moons.slice(1);
-
+const getClosestToPlanet = (moons) => {
+  let [closest, ...rest] = moons;
   for (let moon of rest) {
     if (moon.distanceFromPlanetInKm < closest.distanceFromPlanetInKm) {
       closest = moon;
     }
   }
-
   return closest.name;
-}
+};
 
 /*
   To update this function, you should:
@@ -56,7 +52,7 @@ function getClosestToPlanet(moons) {
 
   You should not need to make any other changes to the code.
 */
-function createMoon(name, planet, distanceFromPlanetInKm) {
+const createMoon = (name, planet, distanceFromPlanetInKm = "Unknown") => {
   if (!name || !planet) {
     return "Name and planet are required.";
   }
@@ -64,11 +60,11 @@ function createMoon(name, planet, distanceFromPlanetInKm) {
   distanceFromPlanetInKm = distanceFromPlanetInKm || "Unknown";
 
   return {
-    name: name,
-    planet: planet,
-    distanceFromPlanetInKm: distanceFromPlanetInKm,
+    name,
+    planet,
+    distanceFromPlanetInKm,
   };
-}
+};
 
 module.exports = {
   organizeMoonsByPlanet,
